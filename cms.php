5ff7899889116535ca2ed9384b31403a6fbcd04d
adam <baker0169@gmail.com>
2017-01-29 21:48:54 +0800

    new

--- cms.php
+++ cms.php
@@ -9,21 +9,24 @@
 		<div class="block">
 			<div class="header">
 				<ul class="menu-list">
-					<li class= "menu-item"><a href="">New Article</a></li>
-					<li class= "menu-item"><a href="">Manage Article</a></li>
+					<li class= "menu-item"><a href="">新建文章</a></li>
+					<li class= "menu-item"><a href="">文章管理</a></li>
 					<li class= "clearfix"></li>
 				</ul>
 			</div>
+			<div class="separator"></div>
 		</div>
 		<div class="block">
 			<form class="form" action= 'thx.php' method = "post">
 				<div class="box"><input type="text" name="title" placeholder="name"></div>
+				<div class="box"><input type="text" name="author" placeholder="author"></div>
+				<div class="box" ><textarea class= "txtarea-1" name = "des" placeholder="description"></textarea></div>
 
-				<div class="box" ><textarea class= "txtarea" name = "txt" placeholder="text you like"></textarea></div>
-				<div class="box"><input type="submit"></div>
+				<div class="box" ><textarea class= "txtarea" name = "txt" placeholder="your article"></textarea></div>
+				<div class="box"><input type="submit" class="sbmt"></div>
 			</form>
 		</div>
 	</div>
 
 </body>
-</html>	
+</html>
